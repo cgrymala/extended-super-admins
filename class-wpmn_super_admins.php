51aca0adffa6a9ce6ec566b9c7b4b4a224a1dbea
cgrymala <cgrymala@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-03 21:48:11 +0000

    First commit of this plugin.
    git-svn-id: http://svn.wp-plugins.org/extended-super-admins/trunk@340456 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class-wpmn_super_admins.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Classes and methods for extended super admins in a multi-network setup
+ * @package WordPress
+ * @subpackage ExtendedSuperAdmins
+ */
+
+if( !class_exists( 'extended_super_admins' ) )
+	require_once( 'class-extended_super_admins.php' );
+
+if( class_exists( 'extended_super_admins' ) && !class_exists( 'wpmn_super_admins' ) ) {
+	/**
+	 * The class for extended super admins in a WordPress Multi-Network setup
+	 * @package WordPress
+	 * @subpackage ExtendedSuperAdmins
+	 */
+	class wpmn_super_admins extends extended_super_admins {
+		
+		var $multi_network_admins = array();
+		
+		function __construct() {
+			if( !parent::__construct() )
+				return false;
+			
+			$this->can_manage_plugin();
+			
+			add_action( 'plugins_loaded', 'set_multi_network_admins' );
+			add_filter('network_admin_plugin_action_links_' . ESA_PLUGIN_BASENAME, array($this, 'add_settings_link'));
+		}
+		
+		function add_settings_link( $links ) {
+			$links = parent::add_settings_link( $links );
+			
+			global $wp_version;
+			$options_page = ( version_compare( $wp_version, '3.0.9', '>' ) ) ? 'settings' : 'ms-admin';
+			
+			$multi_network_activate_link = '<br/><a href="' .
+				wp_nonce_url( $options_page .
+				'.php?options-action=multi_network_activate&page=' .
+				ESA_OPTIONS_PAGE, '_esa_multi_network' ) .
+				'">' .
+				__( 'Multi-Network Activate', ESA_TEXT_DOMAIN ) .
+				'</a>';
+			$multi_network_deactivate_link = '<a href="' .
+				wp_nonce_url( $options_page .
+				'.php?options-action=multi_network_deactivate&page=' .
+				ESA_OPTIONS_PAGE, '_esa_multi_network' ) .
+				'">' .
+				__( 'Deactivate On All Networks', ESA_TEXT_DOMAIN ) .
+				'</a>';
+			
+			array_push( $links, $multi_network_activate_link, $multi_network_deactivate_link );
+			return $links;
+		}
+		
+		function can_manage_plugin() {
+			if( $this->perms_checked )
+				return current_user_can( 'manage_esa_options' );
+			
+			global $current_user;
+			get_currentuserinfo();
+			/*$user = new WP_User( $current_user->ID );*/
+			
+			if( $this->is_full_network_admin() )
+				$current_user->add_cap( 'manage_esa_options' );
+			else
+				$current_user->remove_cap( 'manage_esa_options' );
+				
+			return current_user_can( 'manage_esa_options' );
+		}
+		
+		/**
+		 * Get a list of users that are Super Admins on all networks in the installation
+		 *
+		 * Retrieves the lists of Super Admins for each individual network, then checks 
+		 * for the users that are on all of those lists. Returns the array of users that
+		 * are Super Admins for all of the networks.
+		 * @return array list of usres that are super admins for entire set of networks
+		 */
+		function get_super_network_admins() {
+			global $wpdb;
+	
+			$full_network_admins = array();
+			
+			/* Get a list of all networks */
+			$networks = $wpdb->get_results( $wpdb->prepare( "SELECT DISTINCT id FROM " . $wpdb->site ) );
+			$network_admins = array();
+			$all_super_admins = array();
+			$network_admins = $wpdb->get_results( $wpdb->prepare( "SELECT site_id, meta_value FROM " . $wpdb->sitemeta . " WHERE meta_key = 'site_admins'" ) );
+			foreach( $network_admins as $network ) {
+				$all_super_admins = array_merge( $all_super_admins, maybe_unserialize( $network->meta_value ) );
+				$full_network_admins = array_intersect( $all_super_admins, maybe_unserialize( $network->meta_value ) );
+			}
+//			$i = 0;
+//			/* Get lists of all super admins for each network */
+//			if( count( $networks ) ) {
+//				foreach( $networks as $network ) {
+//					/*wpmn_switch_to_network( $network->id );*/
+//					$network_admins[$i] = maybe_unserialize( $wpdb->get_var( $wpdb->prepare( "SELECT meta_value FROM " . $wpdb->sitemeta . " WHERE site_id = " . $network->id . " AND meta_key = 'site_admins'" ) ) );
+//					$all_super_admins = array_merge( $all_super_admins, $network_admins[$i] );
+//					/*wpmn_restore_current_network();*/
+//					$i++;
+//				}
+//			}
+//			$all_super_admins = array_unique( $all_super_admins );
+//			/* Find out which Super Admins are admins on all networks */
+//			foreach( $network_admins as $k=>$v ) {
+//				$full_network_admins = array_intersect( $all_super_admins, $v );
+//				/* If, at any point, our array of full network admins is empty, we return an empty array; since a user obviously can't be a super admin on *all* networks if s/he isn't an admin on the one we just checked */
+//				if( !count($full_network_admins) )
+//					return array();
+//			}
+			
+			return $full_network_admins;
+		}
+		
+		/**
+		 * Check to see if the current user is a full network admin
+		 * @return bool whether or not the user is a full network admin
+		 */
+		function is_full_network_admin() {
+			if( empty( $this->multi_network_admins ) )
+				$this->set_multi_network_admins();
+			
+			global $current_user;
+			get_currentuserinfo();
+			return in_array( $current_user->user_login, $this->multi_network_admins );
+		}
+		
+		function set_multi_network_admins() {
+			if( !is_super_admin() )
+				return;
+				
+			$this->multi_network_admins = $this->get_super_network_admins();
+		}
+		
+		function get_super_admin_list() {
+			if( !empty( $this->all_super_admins ) )
+				return $this->all_super_admins;
+			
+			global $wpdb;
+			$all_super_admins = array();
+			$super_admins = $wpdb->get_results( $wpdb->prepare( "SELECT meta_value FROM " . $wpdb->sitemeta . " WHERE meta_key = 'site_admins'" ) );
+			foreach( $super_admins as $site_admins ) {
+				$site_admins = maybe_unserialize( $site_admins->meta_value );
+				$all_super_admins = array_merge( $all_super_admins, $site_admins );
+			}
+			$this->all_super_admins = array_unique( $all_super_admins );
+			return $this->all_super_admins;
+		}
+		
+		function save_options( $values_to_use=NULL ) {
+			if( empty( $values_to_use ) ) {
+				$this->debug = '<div class="error">';
+				$this->debug .= '<p>' . __( 'The form to save the Extended Super Admin options was submitted, but the values were empty. Therefore, nothing was updated.', ESA_TEXT_DOMAIN ) . '</p>';
+				$this->debug .= '</div>';
+				return;
+			}
+			
+			/*$this->set_options( $values_to_use );
+			$this->options = $this->get_options();*/
+			
+			global $wpdb;
+			$networks = $wpdb->get_results( $wpdb->prepare( "SELECT DISTINCT id FROM " . $wpdb->site ) );
+			$updated = true;
+			foreach( $networks as $network ) {
+				wpmn_switch_to_network( $network->id );
+				$upd = update_site_option( ESA_OPTION_NAME, $values_to_use );
+				if( !$upd )
+					$updated = false;
+				wpmn_restore_current_network();
+			}
+			
+			if( $updated ) {
+				$this->debug = '<div class="updated">';
+				$this->debug .= '<p>' . __( 'The options for the Extended Super Admins plugin have been updated.', ESA_TEXT_DOMAIN ) . '</p>';
+				
+				/*ob_start();
+				var_dump( $this->options );
+				$this->debug .= '<div><pre><code>' . ob_get_contents() . '</code></pre></div>';
+				ob_end_clean();*/
+				
+				$this->debug .= '</div>';
+			} else {
+				$this->debug .= '<div class="error">';
+				$this->debug .= '<p>' . __( 'There was an error committing the changes to the database.', ESA_TEXT_DOMAIN ) . '</p>';
+				
+				/*ob_start();
+				var_dump( $this->options );
+				$this->debug .= '<div><pre><code>' . ob_get_contents() . '</code></pre></div>';
+				ob_end_clean();*/
+				
+				$this->debug .=  '</div>';
+			}
+			unset( $this->options );
+			$this->set_options();
+			return $this->options;
+		}
+		
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,69 @@
+=== Extended Super Admins ===
+Contributors: cgrymala
+Tags: wpmu, multisite, super admins, role manager, capabilities, wpmn
+Requires at least: 3.0
+Tested up to: 3.1-RC3
+Stable tag: 0.1
+
+This plugin allows you to create multiple levels of Super Admins in a multi-site configuration.
+
+== Description ==
+
+The way that WordPress roles and capabilities are set up, there is only level of Super Admin available. Without this plugin, the only way to grant a user control over certain aspects of the network, you have to grant that user control over all aspects.
+
+This plugin allows you to revoke specific privileges from any Super Admin on the network. You can create new roles within the site, then assign any Super Admin to that role; effectively removing the appropriate privileges.
+
+This plugin does not grant any new privileges (with the possible exception of the ability to manage the settings for this plugin itself) to any users. It is only capable of removing privileges.
+
+This plugin is also built to be compatible with the WordPress Multi Network plugin. If the WPMN plugin is active, the options for this plugin will be saved and used for all of the networks within the installation.
+
+== Important Notice ==
+
+So far, this plugin has only been tested on a handful of WordPress installations; all my own. Therefore, it is entirely possible that there could be serious bugs when used in different settings. At this time, I am seeking people to test the plugin, so please report any issues you encounter. Thank you.
+
+== Requirements ==
+
+This plugin requires WordPress. It might work with WordPressMU versions older than 3.0, but it has not been tested with those.
+
+This plugin requires WordPress to be setup in Multi Site mode. It will not do anything at all if the Multi Site functions are not enabled.
+
+This plugin also requires PHP5. Some attempt has been made to make it compatible with PHP4, but it has not been tested in that environment.
+
+== Installation ==
+
+1. Download and unzip the extended_super_admins package.
+1. Upload the 'extended_super_admins' folder to the '/wp-content/plugins/' directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Promote any users that you want to manage through this plugin to Super Admin status
+1. Begin creating new Super Admin roles on the settings page for this plugin, and assign the appropriate users to those new roles
+
+== Frequently Asked Questions ==
+
+= How do I let you know if I find a bug? =
+
+Please either hit me up on Twitter @cgrymala or e-mail me at cgrymala@umw.edu. If you e-mail me, please include "Extended Super Admins" in the subject line of the message. Thanks.
+
+= Can I use this plugin to grant extra capabilities to user(s)? =
+
+No. This plugin is only capable of removing privileges from a user. It cannot add extra privileges.
+
+= Can I use this plugin on a non-Multi-Site WordPress? =
+
+No. This plugin would be pointless for non-Multi-Site installations. It might actually generate errors if you try to use it on a site that doesn't have the multi-site capabilities turned on.
+
+= Can a user belong to more than one role? =
+
+At this time, you can assign a user to multiple roles, but only the first role in the list will have an effect on that user's capabilities. In a future version, I will look into allowing multiple roles to modify a user's capabilities.
+
+= How do I know what the capabilities actually do? =
+
+You can find descriptions of most of the capabilities in the WordPress codex. Unfortunately, there is no definitive explanation for each and every capability, so you may have to do some testing and play around with things a bit to get things working exactly the way you want.
+
+= How does this plugin integrate with the WordPress Multi Network plugin from John James Jacoby? =
+
+If the WPMN plugin is active, this plugin will attempt to use and save any and all of its settings across all of the networks. There is currently no way, with WPMN activated, to use different settings on individual networks.
+
+== Changelog ==
+
+= 0.1 =
+This is the first version.
