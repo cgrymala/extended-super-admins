0b97c47747baa2eb9c9be7609f03438f2b93b511
cgrymala <cgrymala@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-10 20:49:03 +0000

    Made a minor modification to the way the plugin is activated on multiple networks.
    git-svn-id: http://svn.wp-plugins.org/extended-super-admins/trunk@358470 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/multi_network_activation.php
+++ inc/multi_network_activation.php
@@ -17,13 +17,6 @@ echo '
 	<div class="wrap">
 		<h2>' . __('Extended Super Admins - Multi-Network Activation') . '</h2>';
 
-if( !function_exists( 'wpmn_network_exists' ) && !class_exists( 'njsl_Networks' ) ) {
-	if( file_exists( WP_PLUGIN_DIR . '/wp-multi-network/wp-multi-network.php' ) ) {
-		require( WP_PLUGIN_DIR . '/wp-multi-network/wp-multi-network.php' );
-	} elseif( file_exists( WP_PLUGIN_DIR . '/networks-for-wordpress/index.php' ) ) {
-		require( WP_PLUGIN_DIR . '/networks-for-wordpress/index.php' );
-	}
-}
 if( !class_exists( 'wpmn_super_admins' ) ) {
 	require( ESA_ABS_DIR . '/' . 'class-wpmn_super_admins.php' );
 }
